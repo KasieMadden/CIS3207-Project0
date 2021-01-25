45853b576c7afa44fa6f27f5ec6242828409df92
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-01-25 13:50:02 -0500

    adding wgrep file

---
+++ wgrep.c
@@ -0,0 +1,18 @@
+//Kasie E. Madden
+//CIS 3207
+//Professor John Fiore
+//Assignment 0- Part A
+//Date Created 1/25/2021
+
+
+//wgrep: searches for fixed strings or basic regular expressions; patterns
+
+//standard library
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//start of the main function
+int main() {
+
+}//end of main function
