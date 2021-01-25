a0f4a3ea8f6d5ff0652a0dd1c0c446c6a665db5d
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-01-25 14:31:39 -0500

    added for loop to invoke files, created file pointer to open and read called files.Also  added error with code 1 for if a file is a  null

--- wcat.c
+++ wcat.c
@@ -13,22 +13,34 @@
 #include<stdlib.h>
 
 //start of the main function
-int main(){
+
+//pass command lines
+int main(int argc,  char *argv[]){
 
     //loop to invoke each file in turn
+    int i;
+    for(i = 1, i < argc; i++) {
 
         //create a file pointer to open and read file
+        FILE *fp = fopen(argv[i],"r");
 
-            //check if pointer is null
+        //check if pointer is null
+        if(fp == NULL) {
 
-                //if null then print error message
-                    //exit with error code 1 when null
+            //if null then print error message
+            printf("Cannot open file\n");
+            //exit with error code 1 when null
+            exit(1);
 
-                // if true enter else loop
+        }//end of if
 
-                    //loop to read line by line until EOF.
+        // if true enter else loop
 
-                //close file
+        //loop to read line by line until EOF.
+
+        //close file
+
+    }//end of for loop
 
                 //exit program with code 0
 
