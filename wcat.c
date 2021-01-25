b37a2f34f7809b16e5e7a2debd91dd21c9e7b09f
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-01-25 13:38:22 -0500

    pseudo code outlining project

---
+++ wcat.c
@@ -0,0 +1,45 @@
+//Kasie E. Madden
+//CIS 3207
+//Professor John Fiore
+//Assignment 0- Part A
+//Date Created 1/25/2021
+
+
+//wcat: This exercise is to build a simple version of the cat command.
+//      This program will concatenate 2 more from files when called.
+
+//standard library
+#include<stdio.h>
+#include<stdlib.h>
+
+//start of the main function
+int main(){
+
+    //loop to invoke each file in turn
+
+        //create a file pointer to open and read file
+
+            //check if pointer is null
+
+                //if null then print error message
+                    //exit with error code 1 when null
+
+                // if true enter else loop
+
+                    //loop to read line by line until EOF.
+
+                //close file
+
+                //exit program with code 0
+
+
+
+
+
+
+
+
+
+
+}//end of main function
+
