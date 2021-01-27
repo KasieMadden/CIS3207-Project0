f64a8e5a48dcf659ce615af4491319ed4c7d16f3
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-01-27 00:50:06 -0500

    added if for when there are for args on command line, started when for loop to open and read file, will print error if file print is null

---
+++ cmake-build-debug/CMakeFiles/CIS3207_Project0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CIS3207_Project0.exe"
+  "CIS3207_Project0.exe.manifest"
+  "CIS3207_Project0.lib"
+  "CIS3207_Project0.pdb"
+  "CMakeFiles/CIS3207_Project0.dir/wcat.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CIS3207_Project0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
