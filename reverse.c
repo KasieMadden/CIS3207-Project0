0bac9bdc1454f14917038e441dd447545d2940f4
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-02-03 11:44:48 -0500

    added more comments

--- reverse.c
+++ reverse.c
@@ -21,7 +21,7 @@
 //https://stackoverflow.com/questions/1919975/creating-a-stack-of-strings-in-c
 
 
-// Stack structure for node
+// Stack structure - Node
 struct stackNode{
     char *data;
     struct  stackNode *next;
@@ -29,13 +29,15 @@ struct stackNode{
 };//end of stackNode
 
 
-
+// Stack structure - head
 struct stackHead{
     struct stackNode *head;
     size_t size;
 
 };//end of stackHead
 
+
+// creating the stack instance
 struct stackHead *newStack(void){
     struct stackHead *stack = malloc(sizeof *stack);
         if(stack){
@@ -69,6 +71,7 @@ void push(struct stackHead *stack, char *value ){
 
 }//end push
 
+//function for the top of the stack
 char *top( struct stackHead *stack){
     if(stack && stack->head){
         return stack->head->data;
@@ -94,28 +97,41 @@ void pop(struct stackHead *stack){
 
 
 
-
+// main function
 int main(int argc,  char *argv[]){
 
     //if no args passed (1)=  stdin -> stdout
     if(argc == 1){
 
-        //initializing for s
+        //initializing stack pointer
+        //seting string to NuLL
+        //setting buffer size
         struct stackHead *sp = newStack();
         char *string = NULL;
         size_t buff = 0;
 
+
+        //print statement to prompt user to
         printf("Input Sting to be reversed: \n");
+
+        //getline is getting each line
         getline(&string, &buff ,stdin);
 
 
+        //while string is comparing to new line
+        //pushes on stack and gets next line
         while(strcmp(string, "\n") != 0){
             push(sp, string);
             getline(&string, &buff ,stdin);
 
         }//end of while
 
+
+        // print statement letting user know input
         printf("Output is:\n");
+
+        //while the top of stack is not null
+        //will keep popping and printing the top line on stack
         while(top(sp) != NULL) {
             printf("%s",top(sp));
             pop(sp);
@@ -128,23 +144,34 @@ int main(int argc,  char *argv[]){
     //pass (2) args = file -> stdout
     if(argc == 2){
 
+        //initializing file pointer pointer - read
+        //initializing stack pointer
+        //setting string to NuLL
+        //setting buffer size
         FILE *fp = fopen(argv[1],"r");
         struct stackHead *sp = newStack();
         char *string = NULL;
         size_t buff = 0;
 
+
+        //error message if file cant open
         if( fp == NULL){
             printf("Error: Cannot open file\n");
             exit(1);
         }//end of if for error
 
-
+            // using the same idea of  argc ==1
+            //replace stdin with file pointer
             while (getline(&string, &buff, fp) != -1) {
                 printf("%s", string);
                 push(sp, string);
-                //getline(&string, &buff, stdin);
             }//end of while
 
+
+
+        //while the top of stack is not null
+        //will keep popping and printing the top line on stack
+        //using fgets to a file open for writing
             printf("Output is:\n");
             while (top(sp) != NULL) {
                 printf("%s", top(sp));
@@ -165,31 +192,49 @@ int main(int argc,  char *argv[]){
     //If (3) args passed = file to file
     if(argc == 3){
 
+
+        //initializing file pointer  - read
+        //initializing temp file pointer  - write
+        //initializing stack pointer
+        //setting string to NuLL
+        //setting buffer size
         FILE *fp = fopen(argv[1],"r");
         FILE *tp = fopen(argv[2], "w");
         struct stackHead *sp = newStack();
         char *string = NULL;
         size_t buff = 0;
 
+
+        //error message if file cant open
         if( fp == NULL){
             printf("Error: Cannot open file\n");
             exit(1);
         }//end of if for error
 
 
+
+        // using the same idea of  argc == 1
+        //stayed the same as arg==2
+        //replace stdin with file pointer
         while (getline(&string, &buff, fp) != -1) {
             printf("%s", string);
             push(sp, string);
-            //getline(&string, &buff, stdin);
         }//end of while
 
-       // printf("Output is:\n");
+
+
+        //while the top of stack is not null
+        //will keep popping and printing the top line on stack
+        //using fgets to a file open for writing
+        //placing it into temp file pointer
         while (top(sp) != NULL) {
             printf("%s", top(sp));
             fputs(top(sp), tp);
             pop(sp);
         }//end of while
 
+        //close file
+        //successful exit code
         fclose(fp);
         exit(0);
 
@@ -199,6 +244,8 @@ int main(int argc,  char *argv[]){
 
 
     // (>3) is an error
+    //anymore than 3 args will run an error
+    //error code 1
     else if(argc > 3){
         fprintf(stderr,"usage: reverse <input> <output>/n");
         exit(1);
